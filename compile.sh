920ab03f2d5e74143d400690631e4fdfed3687ba
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-03-29 21:07:32 +0900

    update docker image  && settting font

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 rm dist/domain.png &&\
- cat src/domain.pu | docker run --rm -i think/plantuml -charset UTF-8 -tpng > dist/domain.png &&\
+ cat src/domain.pu | docker run --rm -i curelemonade/plantuml -tpng > dist/domain.png &&\
   git add dist/domain.png 
